6baa0b43d868a9a880ed2989170f53f52a1a24c6
Tinram <Tinram@users.noreply.github.com>
2017-01-17 21:10:27 +0000

    update cmdline_example.php

--- cmdline_example.php
+++ cmdline_example.php
@@ -10,7 +10,7 @@
     *
     * @author        Martin Latter <copysense.co.uk>
     * @copyright     Martin Latter 15/06/2016
-    * @version       0.1
+    * @version       0.11
     * @license       GNU GPL v3.0
     * @link          https://github.com/Tinram/File-Identifier.git
 */
@@ -27,7 +27,8 @@ use CopySense\FileIdentifier\FileIdentifier;
 
 if (@ ! $_SERVER['argv'][1])
 {
-    $sUsage = PHP_EOL . basename($_SERVER['argv'][0], '.php') . PHP_EOL . PHP_EOL . "\tusage: " . basename($_SERVER['argv'][0], '.php') . ' <filename>' . PHP_EOL . PHP_EOL;
+    $sUsage = PHP_EOL . ' ' . basename(__FILE__, '.php') . PHP_EOL . PHP_EOL . "\tusage: php -f " . basename(__FILE__) . ' <filename>' . PHP_EOL . PHP_EOL;
+
     die($sUsage);
 }
 
@@ -35,7 +36,7 @@ $sFile = $_SERVER['argv'][1];
 
 if ( ! file_exists($sFile))
 {
-    die(PHP_EOL . $sFile . ' does not exist in this directory!' . PHP_EOL . PHP_EOL);
+    die('\'' . $sFile . '\' does not exist in this directory!' . PHP_EOL);
 }
 else
 {
